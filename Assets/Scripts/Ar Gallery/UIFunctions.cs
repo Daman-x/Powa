7dbd2019850963ff08c565a04f418498eb77254b
Daman-x <dogmacreations.x@gmail.com>
2021-05-09 14:52:28 +0530

    Updated to 2020.3.7

---
+++ Assets/Scripts/Ar Gallery/UIFunctions.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFunctions : MonoBehaviour
+{
+
+    private GameObject Canvas;
+    public GameObject Options;
+    private GameObject UIOptions;
+
+    private bool Isclose = true;
+
+    private void Start()
+    {
+        Canvas = GameObject.Find("Canvas");
+        UIOptions = Canvas.transform.Find("UIOptions").gameObject;
+    }
+
+    #region side panel functions
+
+    public void OnOptionsClicked()
+    {
+        Debug.Log("she");
+        if(Isclose)
+        {
+            Debug.Log("her");
+            UIOptions.SetActive(true);
+          //  UIOptions.transform.position = new Vector3(-209f, UIOptions.transform.position.y, 0f);
+            Options.transform.position = new Vector3(Options.transform.position.x + 290f, Options.transform.position.y, 0f);
+            Isclose = false;
+        }
+        else
+        {
+            Debug.Log("he");
+            UIOptions.SetActive(false);
+           // UIOptions.transform.position = new Vector3(-209f, UIOptions.transform.position.y, 0f);
+            Options.transform.position = new Vector3(Options.transform.position.x - 290f, Options.transform.position.y, 0f);
+            Isclose = true;
+        }
+       
+    }
+
+    #endregion
+
+}
