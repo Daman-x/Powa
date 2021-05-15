21a88a3390338b6a319953079c8b7603401709b7
Ryo Kanazawa <76740044+PromisedStar@users.noreply.github.com>
2021-05-15 22:53:42 +0900

    Change
    I have displayed registries and items on Home screen.

--- Assets/Scripts/Libs/AppManager/InitApp.cs
+++ Assets/Scripts/Libs/AppManager/InitApp.cs
@@ -22,12 +22,13 @@ namespace Assets.Scripts.Libs.AppManager
         public void init()
         {
             getAllRegistry();
-            getAllItem();
-            getAllArtist();
+            //getAllItem();
+            //getAllArtist();
         }
 
         public void getAllRegistry()
         {
+            if (AppData._registries != null) return;
             string response = GetFromNet.GetApiData("registry");
             RegistryData data1 = JsonUtility.FromJson<RegistryData>("{\"data\":" + response + "}");
             AppData._registries = data1.data;
@@ -35,6 +36,7 @@ namespace Assets.Scripts.Libs.AppManager
 
         public void getAllItem()
         {
+            if (AppData._items != null) return;
             string response = GetFromNet.GetApiData("token/getNewItems?limit=100&id=0");
             ItemData data1 = JsonUtility.FromJson<ItemData>("{\"data\":" + response + "}");
             AppData._items= data1.data;
@@ -42,6 +44,7 @@ namespace Assets.Scripts.Libs.AppManager
 
         public void getAllArtist()
         {
+            if (AppData._artists != null) return;
             string response = GetFromNet.GetApiData("artist/get");
             ArtistData data1 = JsonUtility.FromJson<ArtistData>("{\"data\":" + response + "}");
             AppData._artists = data1.data;
